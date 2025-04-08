b1c452e7da2eacf4edbd00141542bd40c34c16ae
MarcinRod <rodziewicz.marcin@gmail.com>
2025-04-08 20:39:33 +0200

    The project won't build without completing instructions

---
+++ app/src/main/java/com/example/jokeapp/retrofit/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.example.jokeapp.retrofit
+
+
+import androidx.annotation.StringRes
+import com.example.jokeapp.R
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+// TODO: Placeholder for the Retrofit client and API interface
